1f5f37bca34dd7b517ff9b4209728693adc991ab
小小升 <huang3071725244@163.com>
2025-05-16 22:52:59 +0800

    Delete shoping directory

--- shoping/app/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import Dashboard from "./Dashboard/page";
-
-export default function Home() {
-  return (
-    <Dashboard></Dashboard>
-  )
-}
--- shoping/types/index.ts
+++
@@ -1,8 +0,0 @@
-
-export interface DiaryPost {
-    id: number;
-    title: string;
-    body: string;
-    userId: number;
-  }
-  
